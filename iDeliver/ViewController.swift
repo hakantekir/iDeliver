e83bef835030b30b4fd4fbb605567f82399ad1d4
Hakan <hakantekir2002@outlook.com>
2023-02-09 13:38:26 +0300

    Initial Commit

---
+++ iDeliver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iDeliver
+//
+//  Created by Hakan Tekir on 9.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
