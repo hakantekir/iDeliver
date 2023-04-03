e4719c2a5bd651d5b2d06560368c07d4554b2073
Hakan <hakantekir2002@outlook.com>
2023-04-03 13:02:16 +0300

    feat: Create login view model

---
+++ iDeliver/Scenes/Authentication/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewModel.swift
+//  iDeliver
+//
+//  Created by Hakan Tekir on 3.04.2023.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewModel {
+    var coordinator: LoginCoordinator?
+    let manager = LoginManager.shared
+    
+    func login (email: String, password: String) {
+        if (email.isEmpty || password.isEmpty) {
+            let alert = AlertViewHelper.showAlert(title: "Missing Credentials", message: "Please provide a name, email, and password")
+            self.coordinator?.presentAlert(alert, animated: true, completion: nil)
+            return
+        }
+        var user = User(email: email, password: password)
+        manager.loginRequest(user: user) { response in
+            switch response {
+            case .success(let response):
+                user.token = response.token
+            case .failure(let failure):
+                DispatchQueue.main.sync {
+                    let alert = AlertViewHelper.showAlert(title: failure.reason ?? "Error", message: failure.message ?? "Error")
+                    self.coordinator?.presentAlert(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
