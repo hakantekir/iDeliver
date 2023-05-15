3e1c7658824b8f79ccbfe5792f66c34bc3bd14c5
Hakan <hakantekir2002@outlook.com>
2023-05-15 12:20:35 +0300

    fix: Prevent app crash due to incorrect image name and force casting

--- iDeliver/Scenes/Authentication/Loading/Model/LoadingSlide.swift
+++ iDeliver/Scenes/Authentication/Loading/Model/LoadingSlide.swift
@@ -16,11 +16,11 @@ struct LoadingSlide {
     
     static let loadingContenData: [LoadingSlide] = [
         
+        LoadingSlide(imageView: UIImage(named: "image0")! , header: "Find Your Dream Job Here!", description: "Explore all the most exciting job roles based on your interest and study major."),
+        
         LoadingSlide(imageView: UIImage(named: "image1")! , header: "Find Your Dream Job Here!", description: "Explore all the most exciting job roles based on your interest and study major."),
         
         LoadingSlide(imageView: UIImage(named: "image2")! , header: "Find Your Dream Job Here!", description: "Explore all the most exciting job roles based on your interest and study major."),
-        
-        LoadingSlide(imageView: UIImage(named: "image3")! , header: "Find Your Dream Job Here!", description: "Explore all the most exciting job roles based on your interest and study major."),
         
     ]
     
